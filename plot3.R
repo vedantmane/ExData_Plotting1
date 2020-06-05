34428d144647aa3f7fe7ca7645002a0996e0e76a
Vedant Mane <vedant12mane@gmail.com>
2020-06-05 16:16:30 +0530

    Final commit - Plot 3 complete

---
+++ plot3.R
@@ -0,0 +1,12 @@
+data <- read.table("../Power Consumption/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dataSet <- data[which(data$Date == as.Date("2007-02-01", "%Y-%m-%d") | data$Date == as.Date("2007-02-02", "%Y-%m-%d")), ]
+par(mfrow = c(1,1), mar = c(4,4,2,1))
+dateTime <- paste(dataSet$Date, dataSet$Time)
+dataSet$DateTime <- as.POSIXct(dateTime)
+plot(dataSet$DateTime, dataSet$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
+lines(dataSet$DateTime, dataSet$Sub_metering_2, col = "orangered")
+lines(dataSet$DateTime, dataSet$Sub_metering_3, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "orangered", "blue"), lty = 1)
+dev.copy(png, filename = "plot3.png")
+dev.off()
