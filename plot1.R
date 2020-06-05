0fb8bff7c04324d14a4d9d9f9c1b94250de40216
Vedant Mane <vedant12mane@gmail.com>
2020-06-05 14:27:57 +0530

    Final commit - Plot 1 complete

--- plot1.R
+++ plot1.R
@@ -3,5 +3,5 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 dataSet <- data[which(data$Date == as.Date("2007-02-01", "%Y-%m-%d") | data$Date == as.Date("2007-02-02", "%Y-%m-%d")), ]
 par(mfrow = c(1,1), mar = c(4,4,2,1))
 hist(as.numeric(dataSet$Global_active_power), ylim = c(0,1200), xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "orangered")
-png("plot1.png", width = 480, height = 480)
+dev.copy(png, file = "plot1.png")
 dev.off()
