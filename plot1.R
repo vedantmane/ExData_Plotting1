2939d41cb984a0ba533a27f5b5dc920b1ca3e66b
Vedant Mane <vedant12mane@gmail.com>
2020-06-05 12:40:29 +0530

    Plot 1 Complete - Final Commit

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data <- read.table("../Power Consumption/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dataSet <- data[which(data$Date == as.Date("2007-02-01", "%Y-%m-%d") | data$Date == as.Date("2007-02-02", "%Y-%m-%d")), ]
+par(mfrow = c(1,1), mar = c(4,4,2,1))
+hist(as.numeric(dataSet$Global_active_power), ylim = c(0,1200), xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "orangered")
+png("plot1.png", width = 480, height = 480)
+dev.off()
