bb10542db134542bf5c400e0624758a7c42010a6
Vedant Mane <vedant12mane@gmail.com>
2020-06-05 15:43:47 +0530

    Final commit - Plot 2 complete

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table("../Power Consumption/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dataSet <- data[which(data$Date == as.Date("2007-02-01", "%Y-%m-%d") | data$Date == as.Date("2007-02-02", "%Y-%m-%d")), ]
+par(mfrow = c(1,1), mar = c(4,4,2,1))
+dateTime <- paste(dataSet$Date, dataSet$Time)
+dataSet$DateTime <- as.POSIXct(dateTime)
+plot(dataSet$DateTime, as.numeric(dataSet$Global_active_power), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
